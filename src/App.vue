87788f5000065f46f2eee922409c009773654ed8
akatsuura <a.katsuura1991@gmail.com>
2016-12-17 17:09:38 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+
+    <Midi></Midi>
+  </div>
+</template>
+
+<script>
+import Midi from './components/Midi'
+export default {
+  name: 'app',
+  components: {
+    Midi
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Midi.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class='Midi'>
+    {{midiDevices.inputs}}
+    {{out}}
+    <div v-for='v in inputs'>
+      <label>{{v.num}}</label>
+      <input v-bind:value='v.value' max='127' min='0' type='range' ></input>
+    </div>
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'midi-control',
+  data () {
+    return {
+      out: '',
+      midiDevices: {
+        inputs: {}
+      },
+      inputs: {}
+    }
+  },
+  mounted: function () {
+    let self = this
+    ConnectMidiController()
+    // controllerを紐付けるよ
+    function ConnectMidiController () {
+      // requestMIDIAccessはpromise型を返すのでthen()で成功した時と失敗した時の処理のコールバックを登録する
+      navigator.requestMIDIAccess().then(requestSuccess, requestError)
+    }
+    function requestSuccess (data) {
+      console.log('this', this)
+      console.log('つながったよ', data)
+      data.inputs.forEach(function (device) {
+        // デバイス情報を保存
+        self.midiDevices.inputs[device.name] = device
+        // MIDIメッセージイベント受信したときの登録
+        device.addEventListener('midimessage', inputEvent, false)
+      })
+    }
+    // 入力が会ったときの処理
+    function inputEvent (e) {
+      self.inputs[e.data[1]] = {num: e.data[1], value: e.data[2]}
+      console.log(self.inputs)
+      self.$forceUpdate()
+    }
+    // エラーの時の処理
+    function requestError (error) {
+      console.error(error)
+    }
+  }
+}
+</script>
